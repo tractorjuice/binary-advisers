53a5f659dce7d9643a629cd6bf26634d3e714805
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-03 23:50:21 +0000

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -1,5 +1,6 @@
 import os
 import re
+import uuid
 import openai
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
@@ -13,6 +14,7 @@ from streamlit_player import st_player
 # Set OpenAI Model and API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 os.environ["PROMPTLAYER_API_KEY"] = st.secrets["PROMPTLAYER"]
+
 #MODEL = "gpt-3"
 #MODEL = "gpt-3.5-turbo"
 #MODEL = "gpt-3.5-turbo-0613"
@@ -21,6 +23,9 @@ MODEL = "gpt-3.5-turbo-16k-0613"
 #MODEL = "gpt-4"
 #MODEL = "gpt-4-0613"
 #MODEL = "gpt-4-32k-0613"
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
 # Remove HTML from sources
 def remove_html_tags(text):
@@ -43,18 +48,22 @@ def clean_text(text):
     text = remove_markdown(text)
     return text
 
-st.set_page_config(page_title="Chat with Wardley Videos")
-st.title("Chat with Wardley Videos")
+st.set_page_config(page_title="Chat with Binary Advisors")
+st.title("Chat with Binary Advisors")
 st.sidebar.markdown("# Query Videos using AI")
-st.sidebar.divider()
-st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
 st.sidebar.markdown("Current Version: 1.0.0")
 st.sidebar.divider()
-st.sidebar.markdown("Using GPT-4 API")
-st.sidebar.markdown("Uses FAISS")
-st.sidebar.markdown("May run out of OpenAI credits")
-st.sidebar.divider()
-st.sidebar.markdown("Wardley Mapping is provided courtesy of Simon Wardley and licensed Creative Commons Attribution Share-Alike.")
+# Check if the user has provided an API key, otherwise default to the secret
+user_openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", placeholder="sk-...", type="password")
+
+if user_openai_api_key:
+    # If the user has provided an API key, use it
+    # Swap out openai for promptlayer
+    promptlayer.api_key = st.secrets["PROMPTLAYER"]
+    openai = promptlayer.openai
+    openai.api_key = user_openai_api_key
+else:
+    st.warning("Please enter your OpenAI API key", icon="⚠️")
 
 # Get datastore
 DATASTORE = "datastore"
@@ -68,9 +77,8 @@ else:
     st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
 
 system_template="""
-    As a chatbot, analyze the provided videos on Wardley Mapping and offer insights and recommendations.
+    As a chatbot, analyze the provided videos on AI and offer insights and recommendations.
     Suggestions:
-    Explain the analysis process for a Wardley Map
     Discuss the key insights derived from the videos
     Provide recommendations based on the analysis
     Use the following pieces of context to answer the users question.
@@ -87,14 +95,14 @@ chain_type_kwargs = {"prompt": prompt}
 llm = PromptLayerChatOpenAI(
     model_name=MODEL,
     temperature=0,
-    max_tokens=2000,
-    pl_tags=["wardleygptv2"],
-)  # Modify model_name if you have access to GPT-4
+    max_tokens=1000,
+    pl_tags=["binary-advisors-chat", st.session_state.session_id],
+)
 
 chain = RetrievalQAWithSourcesChain.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 3}), # Use MMR search and return 5 (max 20) video sources
+    retriever=vector_store.as_retriever(search_kwargs={"k": 50}),
     return_source_documents=True,
     chain_type_kwargs=chain_type_kwargs
 )

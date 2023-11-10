e1fd14ee12d8010ce41a313d06b4375783e274b8
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-10 09:03:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -63,24 +63,26 @@ elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == 
 
     st.sidebar.write("Messages:" ,st.session_state.messages)
 
-    # Extract the message content
-    message_content = st.session_state.messages[0].content[0].text
-    annotations = message_content.annotations
-    citations = []
-    
-    # Iterate over the annotations and add footnotes
-    for index, annotation in enumerate(annotations):
-        # Replace the text with a footnote
-        message_content.value = message_content.value.replace(annotation.text, f' [{index}]')
-    
-        # Gather citations based on annotation attributes
-        if (file_citation := getattr(annotation, 'file_citation', None)):
-            cited_file = client.files.retrieve(file_citation.file_id)
-            citations.append(f'[{index}] {file_citation.quote} from {cited_file.filename}')
-        elif (file_path := getattr(annotation, 'file_path', None)):
-            cited_file = client.files.retrieve(file_path.file_id)
-            citations.append(f'[{index}] Click <here> to download {cited_file.filename}')
-            # Note: File download functionality not implemented above for brevity
+    for thread_message in st.session_state.messages.data:
+        # Each thread_message's content is a list of MessageContentText objects
+        for message_content in thread_message.content.text:
+
+            annotations = message_content.annotations
+            citations = []
+            
+            # Iterate over the annotations and add footnotes
+            for index, annotation in enumerate(annotations):
+                # Replace the text with a footnote
+                message_content.value = message_content.value.replace(annotation.text, f' [{index}]')
+            
+                # Gather citations based on annotation attributes
+                if (file_citation := getattr(annotation, 'file_citation', None)):
+                    cited_file = client.files.retrieve(file_citation.file_id)
+                    citations.append(f'[{index}] {file_citation.quote} from {cited_file.filename}')
+                elif (file_path := getattr(annotation, 'file_path', None)):
+                    cited_file = client.files.retrieve(file_path.file_id)
+                    citations.append(f'[{index}] Click <here> to download {cited_file.filename}')
+                    # Note: File download functionality not implemented above for brevity
 
     # Add footnotes to the end of the message before displaying to user
     message_content.value += '\n' + '\n'.join(citations)
